81c70c3e75e3f396a794a0325b3a3aae37608b40
SuperFlea2828 <stephen.ancliffe@gmail.com>
2017-09-23 09:06:23 -0400

    First script commit

---
+++ Get-HostEntry.ps1
@@ -0,0 +1,39 @@
+<#
+
+.SYNOPSIS
+This is a simple script that tries to look up a host name using an IP. 
+
+.DESCRIPTION
+Looks up a host name using an IP addres provided either from the command line or pipe line. 
+
+.EXAMPLE
+./Get-HostEntry.ps1
+
+.NOTES
+Authour: Stephen Ancliffe
+
+
+#>
+
+Param(
+    [Parameter(
+    Mandatory = $true,
+    ValueFromPipeline = $true,
+    HelpMessage = "Enter one or more IP's names separated by commas."
+    )]
+    [ValidateLength(7,14)]
+    [string[]]$IPList
+)
+
+foreach ($IP in $IPList){
+    try {
+        $DNSResult = [System.Net.DNS]::GetHostEntry($IP)
+        $DNSResult = $DNSResult.HostName
+        Write-Host "Host name is $DNSResult for IP $IP"   
+    } catch {
+        $DNSResult = "Unable to look up host for "
+        Write-Host $DNSResult -NoNewline
+        Write-Host $IP -ForegroundColor Red
+    }
+}
+
